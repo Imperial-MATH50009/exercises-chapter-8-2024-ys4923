6a4b98ae956f95c6138551becf665a9b9968930f
ys4923 <149376803+ys4923@users.noreply.github.com>
2024-12-11 12:59:38 +0000

    i hate flake8 :(

--- scripts/tests_report_mfe.py
+++ scripts/tests_report_mfe.py
@@ -1,2 +1,5 @@
+"""Write a docstring."""
+
 import time
-time.strptime('14:41:32+08:00', '%H:%M:%S') > time.strptime('14:40:00', '%H:%M:%S')
+time.strptime('14:41:32+08:00', '%H:%M:%S') > time.strptime('14:40:00',
+                                                            '%H:%M:%S')
